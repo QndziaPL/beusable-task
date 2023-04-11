68bee06f49cd3d5d3edc8db5e604191783db5088
Rogacewicz, Maksymilian (External) <maksymilian.rogacewicz@gmail.com>
2023-04-12 00:18:12 +0200

    base setup

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React, { type FC } from "react";
+import "./App.css";
+import { Header } from "../Header/Header";
+
+const App: FC = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import { type FC } from "react";
+import "./Header.css";
+
+export const Header: FC = () => (
+  <h1 className="mainHeader" role="heading">
+    Smart Host
+  </h1>
+);
